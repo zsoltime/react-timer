34e3bb62e122c00fafe064469519f30a8c39d551
Zsolt Meszaros <zsolt@sowad.com>
2017-03-08 09:33:20 +0100

    Add production config to reduce bundle size

---
+++ webpack.prod.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+
+const babelSettings = {
+  presets: ['react', 'es2015', 'stage-0'],
+};
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/js/foundation.min.js',
+    './app/app.jsx',
+  ],
+  externals: {
+    jquery: 'jQuery',
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true,
+        screw_ie8: true,
+      },
+      output: {
+        comments: false,
+        screw_ie8: true,
+      },
+      sourceMap: false,
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Timer: 'app/components/Timer.jsx',
+      Countdown: 'app/components/Countdown.jsx',
+      CountdownForm: 'app/components/CountdownForm.jsx',
+      Clock: 'app/components/Clock.jsx',
+      Controls: 'app/components/Controls.jsx',
+      applicationStyles: 'app/styles/app.sass',
+      formatters: 'app/utils/formatters.js',
+    },
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    loaders: [
+      {
+        loaders: [
+          `babel-loader?${JSON.stringify(babelSettings)}`,
+          'eslint-loader',
+        ],
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+      },
+    ],
+  },
+};
